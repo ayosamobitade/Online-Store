d3700813509ad74e0ee828ef961efdf134ae108c
UncleAyo <ayosamobitade@gmail.com>
2022-07-24 23:22:44 +0100

    store view

--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ def product_list(request, category_slug= None):
     products = Product.objects.filter(available = True)
     if category_slug:
         category = get_object_or_404(Category, slug = category_slug)
-        product = products.filter(category = category)
+        products = products.filter(category = category)
 
     return render(request, 'store/product/list.html', {
         'category':category,
